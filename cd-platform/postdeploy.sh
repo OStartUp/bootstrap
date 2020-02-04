162cbddef22fefef6088973b714db86c93cb0ec6
mcaro <marcelo_carovargas@mcafee.com>
2020-02-04 14:45:52 -0800

    Inc

--- cd-platform/postdeploy.sh
+++ cd-platform/postdeploy.sh
@@ -24,7 +24,7 @@ echo "http://localhost:8001/api/v1/namespaces/$NAMESPACE/services/http:spin-deck
 echo "http://localhost:8001/api/v1/namespaces/$NAMESPACE/services/http:cdplatform-jenkins:8080/proxy/"
 
 kubectl get secret $(kubectl get sa cdplatform-kubernetes-dashboard -n $NAMESPACE -o jsonpath='{.secrets[0].name}') -n $NAMESPACE -o jsonpath='{.data.token}' | base64 -d
-
+echo ""
 echo "Waiting for Jenkins"
 kubectl wait pod -l "app.kubernetes.io/component=jenkins-master" --for=condition=Ready -n $NAMESPACE --timeout=300s
 
